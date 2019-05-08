04e449f715c1b933e71023dd35cd89e7a16ee027
davidmagnussson <davidmagnusson97@gmail.com>
2019-05-08 10:54:25 +0200

    Skelett

--- app.js
+++ app.js
@@ -1,153 +1,151 @@
-var app = (function () {
-    // Application object.
-    var app = {};
+// JavaScript code for the Arduino Beacon example app.
 
-    // Specify your beacon 128bit UUIDs here.
-    var regions =
-        [
-            // Estimote Beacon factory UUID.
-            { uuid: 'B9407F30-F5F8-466E-AFF9-25556B57FE6D' },
-            // Sample UUIDs for beacons in our lab.
-            { uuid: 'F7826DA6-4FA2-4E98-8024-BC5B71E0893E' },
-            { uuid: '8DEEFBB9-F738-4297-8040-96668BB44281' },
-            { uuid: 'A0B13730-3A9A-11E3-AA6E-0800200C9A66' },
-            { uuid: 'E20A39F4-73F5-4BC4-A12F-17D1AD07A961' },
-            { uuid: 'A4950001-C5B1-4B44-B512-1370F02D74DE' },
-            { uuid: '585CDE93-1B01-42CC-9A13-25009BEDC65E' },	// Dialog Semiconductor.
-        ];
+// Application object.
+var app = {};
 
-    // Background detection.
-    var notificationID = 0;
-    var inBackground = false;
-    document.addEventListener('pause', function () { inBackground = true });
-    document.addEventListener('resume', function () { inBackground = false });
+// Regions that define which page to show for each beacon.
+app.beaconRegions = [
+    {
+        id: "page-actors",
+        uuid: "b9407f30-f5f8-466e-aff9-25556b57fe6d",
+        major: 56506,
+        minor: 14941
+    },
+    {
+        id: "page-poets",
+        uuid: "b9407f30-f5f8-466e-aff9-25556b57fe6d",
+        major: 57272,
+        minor: 20467
+    },
+    {
+        id: "page-island",
+        uuid: "b9407f30-f5f8-466e-aff9-25556b57fe6d",
+        major: 22460,
+        minor: 60720
+    }
+];
 
-    // Dictionary of beacons.
-    var beacons = {};
+// Currently displayed page.
+app.currentPage = "page-default";
 
-    // Timer that displays list of beacons.
-    var updateTimer = null;
+app.initialize = function () {
+    document.addEventListener("deviceready", app.onDeviceReady, false);
+    app.gotoPage(app.currentPage);
+};
 
-    app.initialize = function () {
-        document.addEventListener(
-            'deviceready',
-            function () { evothings.scriptsLoaded(onDeviceReady) },
-            false);
+// Called when Cordova are plugins initialised,
+// the iBeacon API is now available.
+app.onDeviceReady = function () {
+    // Specify a shortcut for the location manager that
+    // has the iBeacon functions.
+    window.locationManager = cordova.plugins.locationManager;
+
+    // Start tracking beacons!
+    app.startScanForBeacons();
+};
+
+app.startScanForBeacons = function () {
+    //console.log('startScanForBeacons')
+
+    // The delegate object contains iBeacon callback functions.
+    var delegate = new cordova.plugins.locationManager.Delegate();
+
+    delegate.didDetermineStateForRegion = function (pluginResult) {
+        //console.log('didDetermineStateForRegion: ' + JSON.stringify(pluginResult))
     };
 
-    function onDeviceReady() {
-        // Specify a shortcut for the location manager holding the iBeacon functions.
-        window.locationManager = cordova.plugins.locationManager;
+    delegate.didStartMonitoringForRegion = function (pluginResult) {
+        //console.log('didStartMonitoringForRegion:' + JSON.stringify(pluginResult))
+    };
 
-        // Start tracking beacons!
-        startScan();
+    delegate.didRangeBeaconsInRegion = function (pluginResult) {
+        //console.log('didRangeBeaconsInRegion: ' + JSON.stringify(pluginResult))
+        app.didRangeBeaconsInRegion(pluginResult);
+    };
 
-        // Display refresh timer.
-        updateTimer = setInterval(displayBeaconList, 500);
+    // Set the delegate object to use.
+    locationManager.setDelegate(delegate);
+
+    // Start monitoring and ranging our beacons.
+    for (var r in app.beaconRegions) {
+        var region = app.beaconRegions[r];
+
+        var beaconRegion = new locationManager.BeaconRegion(
+            region.id,
+            region.uuid,
+            region.major,
+            region.minor
+        );
+
+        // Start monitoring.
+        locationManager
+            .startMonitoringForRegion(beaconRegion)
+            .fail(console.error)
+            .done();
+
+        // Start ranging.
+        locationManager
+            .startRangingBeaconsInRegion(beaconRegion)
+            .fail(console.error)
+            .done();
+    }
+};
+
+// Display pages depending of which beacon is close.
+app.didRangeBeaconsInRegion = function (pluginResult) {
+    //console.log('numbeacons in region: ' + pluginResult.beacons.length)
+
+    // There must be a beacon within range.
+    if (0 == pluginResult.beacons.length) {
+        return;
     }
 
-    function startScan() {
-        // The delegate object holds the iBeacon callback functions
-        // specified below.
-        var delegate = new locationManager.Delegate();
+    // Our regions are defined so that there is one beacon per region.
+    // Get the first (and only) beacon in range in the region.
+    var beacon = pluginResult.beacons[0];
 
-        // Called continuously when ranging beacons.
-        delegate.didRangeBeaconsInRegion = function (pluginResult) {
-            //console.log('didRangeBeaconsInRegion: ' + JSON.stringify(pluginResult))
-            for (var i in pluginResult.beacons) {
-                // Insert beacon into table of found beacons.
-                var beacon = pluginResult.beacons[i];
-                beacon.timeStamp = Date.now();
-                var key = beacon.uuid + ':' + beacon.major + ':' + beacon.minor;
-                beacons[key] = beacon;
-            }
-        };
+    // The region identifier is the page id.
+    var pageId = pluginResult.region.identifier;
 
-        // Called when starting to monitor a region.
-        // (Not used in this example, included as a reference.)
-        delegate.didStartMonitoringForRegion = function (pluginResult) {
-            //console.log('didStartMonitoringForRegion:' + JSON.stringify(pluginResult))
-        };
+    // console.log("ranged beacon: " + pageId + " " + beacon.major);
 
-        // Called when monitoring and the state of a region changes.
-        // If we are in the background, a notification is shown.
-        delegate.didDetermineStateForRegion = function (pluginResult) {
-            if (inBackground) {
-                // Show notification if a beacon is inside the region.
-                // TODO: Add check for specific beacon(s) in your app.
-                if (pluginResult.region.typeName == 'BeaconRegion' &&
-                    pluginResult.state == 'CLRegionStateInside') {
-                    cordova.plugins.notification.local.schedule(
-                        {
-                            id: ++notificationID,
-                            title: 'Beacon in range',
-                            text: 'iBeacon Scan detected a beacon, tap here to open app.'
-                        });
-                }
-            }
-        };
-
-        // Set the delegate object to use.
-        locationManager.setDelegate(delegate);
-
-        // Request permission from user to access location info.
-        // This is needed on iOS 8.
-        locationManager.requestAlwaysAuthorization();
-
-        // Start monitoring and ranging beacons.
-        for (var i in regions) {
-            var beaconRegion = new locationManager.BeaconRegion(
-                i + 1,
-                regions[i].uuid);
-
-            // Start ranging.
-            locationManager.startRangingBeaconsInRegion(beaconRegion)
-                .fail(console.error)
-                .done();
-
-            // Start monitoring.
-            // (Not used in this example, included as a reference.)
-            locationManager.startMonitoringForRegion(beaconRegion)
-                .fail(console.error)
-                .done();
-        }
+    // If the beacon is close and represents a new page, then show the page.
+    if (
+        beacon.proximity == "ProximityImmediate" &&
+        app.currentPage == "page-default"
+    ) {
+        app.gotoPage(pageId);
+        return;
     }
 
-    function displayBeaconList() {
-        // Clear beacon list.
-        $('#found-beacons').empty();
-
-        var timeNow = Date.now();
-
-        // Update beacon list.
-        $.each(beacons, function (key, beacon) {
-            // Only show beacons that are updated during the last 60 seconds.
-            if (beacon.timeStamp + 60000 > timeNow) {
-                // Map the RSSI value to a width in percent for the indicator.
-                var rssiWidth = 1; // Used when RSSI is zero or greater.
-                if (beacon.rssi < -100) { rssiWidth = 100; }
-                else if (beacon.rssi < 0) { rssiWidth = 100 + beacon.rssi; }
-
-                // Create tag to display beacon data.
-                var element = $(
-                    '<li>'
-                    + '<strong>UUID: ' + beacon.uuid + '</strong><br />'
-                    + 'Major: ' + beacon.major + '<br />'
-                    + 'Minor: ' + beacon.minor + '<br />'
-                    + 'Proximity: ' + beacon.proximity + '<br />'
-                    + 'RSSI: ' + beacon.rssi + '<br />'
-                    + '<div style="background:rgb(255,128,64);height:20px;width:'
-                    + rssiWidth + '%;"></div>'
-                    + '</li>'
-                );
-
-                $('#warning').remove();
-                $('#found-beacons').append(element);
-            }
-        });
+    // If the beacon represents the current page but is far away,
+    // then show the default page.
+    if (
+        (beacon.proximity == "ProximityFar" ||
+            beacon.proximity == "ProximityNear") &&
+        app.currentPage == pageId
+    ) {
+        app.gotoPage("page-default");
+        return;
     }
+};
 
-    return app;
-})();
+app.gotoPage = function (pageId) {
+    app.hidePage(app.currentPage);
+    app.showPage(pageId);
+    app.currentPage = pageId;
+};
 
+app.showPage = function (pageId) {
+    document.getElementById(pageId).style.display = "block";
+    $('.questions').css('color', 'black');
+    $('.questions').css('background-color', 'white');
+    $('.questions').css('font-weight', 'normal');
+};
+
+app.hidePage = function (pageId) {
+    document.getElementById(pageId).style.display = "none";
+};
+
+// Set up the application.
 app.initialize();
