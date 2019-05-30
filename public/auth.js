e84e01105c1bf810181ff06e30f74debd65def89
Martin Mosesson <martin.mosesson@gmail.com>
2019-05-30 19:00:17 +0200

    kommenterat auth.js

--- public/auth.js
+++ public/auth.js
@@ -1,3 +1,4 @@
+// function that clears input fields
 function clearInputFields() {
   document.getElementById("signUpEmail").value = "";
   document.getElementById("signUpUsername").value = "";
@@ -6,19 +7,21 @@ function clearInputFields() {
   document.getElementById("password").value = "";
 }
 
-// listen for auth status changes
+// listen for auth status changes, ie if someone is logged in or logged out. The app then displays different views depending on it.
 auth.onAuthStateChanged(user => {
   if (user) {
+    // if a user is logged in
     console.log("user logged in: ", user);
     showStartScreen();
     setupUI(user);
   } else {
+    // if a user is logged out
     showLoginScreen();
-    // setupUI();
     console.log("user logged out");
   }
 });
 
+// adding an eventlistener for the register button.
 const signupButton = document.getElementById("register");
 signupButton.addEventListener("click", function() {
   console.log("signig up!");
@@ -28,7 +31,7 @@ signupButton.addEventListener("click", function() {
   const userName = document.getElementById("signUpUsername").value;
   const errorMessage = document.getElementById("signupError");
 
-  //signup the user!
+  //signup the user with firebase!
   auth
     .createUserWithEmailAndPassword(email, password)
     .then(cred => {
@@ -46,43 +49,42 @@ signupButton.addEventListener("click", function() {
         });
     })
     .then(() => {
-      clearInputFields();
+      clearInputFields(); //clearing the inputfields
     })
     .catch(err => {
-      errorMessage.innerHTML = err.message;
+      errorMessage.innerHTML = err.message; //displays an error message for the user if the creation failed
     });
 });
 
-// login
+// adding an eventlistener for the login button
 const loginButton = document.getElementById("loginButton");
 loginButton.addEventListener("click", function() {
-  //get user info
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
   const errorMessage = document.getElementById("loginError");
-  console.log(email, password);
 
+  // signing the user in with email and password.
   auth
     .signInWithEmailAndPassword(email, password)
     .then(cred => {
-      console.log(cred);
       errorMessage.innerHTML = " ";
     })
     .then(() => {
-      clearInputFields();
+      clearInputFields(); // clearing input fields
     })
     .catch(err => {
-      errorMessage.innerHTML = err.message;
+      errorMessage.innerHTML = err.message; // displays an error message for the user if the signin failed
     });
 });
 
-//logout
+//adding an eventlistener for the logout button.
 const logout = document.getElementById("logout");
 logout.addEventListener("click", function() {
-  auth.signOut();
-  clearVariables();
+  auth.signOut(); //signing the user out!
+  clearVariables(); // clearing local variables
 });
 
 document.getElementById("logout2").addEventListener("click", function() {
-  auth.signOut();
+  auth.signOut(); //signing the user out!
+  clearVariables(); //clearing local variables
 });
